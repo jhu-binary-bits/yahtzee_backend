f860657f793fb1152c4035c80c1934a733ad7e3d
Readw1 <readwbrown@gmail.com>
2021-08-08 18:48:13 -0400

    undoing some uncessesary changes

--- src/app/state/state_manager.py
+++ src/app/state/state_manager.py
@@ -6,9 +6,7 @@ from events.event import Event
 from state.transcripts.message import Message
 from state.transcripts.transcript import Transcript
 from state.yahtzee.player import Player
-from engine.entities import ScoreType
 
-from itertools import groupby
 
 
 class StateManager:
@@ -88,8 +86,6 @@ class StateManager:
         return self
 
     def publish_current_state(self):
-
-
         if not self.game_engine.game_started:
             data = {
                 "game_started": self.game_engine.game_started,
